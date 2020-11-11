9e242783afb9f2778488436ecf9d47a63a78ef42
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-11 13:46:21 +0000

    Add Spark Handlebars template as a dependency. Create a new file, index.hbs where to put our html code. Change main method to use handlebars template.

--- src/main/java/com/giuseppesilvestro/Main.java
+++ src/main/java/com/giuseppesilvestro/Main.java
@@ -1,10 +1,18 @@
 package com.giuseppesilvestro;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-        get("/", ((request, response) -> "Welcome Students!"));
+        /*
+        https://github.com/perwendel/spark-template-engines/tree/master/spark-template-handlebars
+        this link will show how to use the handlebars template
+         */
+        get("/", (request, response) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
