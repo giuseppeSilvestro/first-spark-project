b819498bd9b952b46bb21d3c6026f2c6ff334b34
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-12 10:22:55 +0000

    Add Filter to stop unregistered students to access /ideas Replace the use of cookie to get the username with Attribute

--- src/main/java/com/giuseppesilvestro/courses/Main.java
+++ src/main/java/com/giuseppesilvestro/courses/Main.java
@@ -17,14 +17,26 @@ public class Main {
 
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
-        /*
-        https://github.com/perwendel/spark-template-engines/tree/master/spark-template-handlebars
-        this link will show how to use the handlebars template
-         */
+        //save the username cookie as attribute
+        before((request, response) -> {
+            if (request.cookie("username") != null) {
+                request.attribute("username", request.cookie("username"));
+            }
+        });
+
+        //stop unregistered students to access /ideas page
+        before("/ideas", (request, response) -> {
+            //TODO: gs - Send message about redirect
+            if (request.attribute("username") == null) {
+                response.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (request, response) -> {
             //create a new model to check and use the user's input saved as cookie
             Map<String, String> model = new HashMap<>();
-            model.put("username", request.cookie("username"));
+            model.put("username", request.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -48,9 +60,8 @@ public class Main {
         post("/ideas", (request, response) -> {
             //get the title from the text field form in /ideas
             String title = request.queryParams("title");
-            //TODO: csd - This username is tied to the cookie implementation
-            //create a new courseIdea object from the title variable and getting the username from the cookie
-            CourseIdea courseIdea = new CourseIdea(title, request.cookie("username"));
+            //create a new courseIdea object from the title variable and getting the username from the attribute
+            CourseIdea courseIdea = new CourseIdea(title, request.attribute("username"));
             //add the course idea to our List
             dao.add(courseIdea);
             //redirect the user to /ideas again, this time it displays also our List of courseIdea
