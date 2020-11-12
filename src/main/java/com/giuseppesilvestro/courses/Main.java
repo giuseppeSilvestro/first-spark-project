a67481a608343fadd3a9e2423ee9456634f2032b
Giuseppe Silvestro <giuseppe.silvestro@live.com>
2020-11-12 09:33:05 +0000

    Add the option to suggest ideas from the users and save them to our List. Add an iteration through the list and show the saved ideas

--- src/main/java/com/giuseppesilvestro/courses/Main.java
+++ src/main/java/com/giuseppesilvestro/courses/Main.java
@@ -1,5 +1,6 @@
 package com.giuseppesilvestro.courses;
 
+import com.giuseppesilvestro.courses.model.CourseIdea;
 import com.giuseppesilvestro.courses.model.CourseIdeaDAO;
 import com.giuseppesilvestro.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -36,5 +37,24 @@ public class Main {
             model.put("username", username);
             return new ModelAndView(model, "sign-in.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (request, response) -> {
+            //get the title from the text field form in /ideas
+            String title = request.queryParams("title");
+            //TODO: csd - This username is tied to the cookie implementation
+            //create a new courseIdea object from the title variable and getting the username from the cookie
+            CourseIdea courseIdea = new CourseIdea(title, request.cookie("username"));
+            //add the course idea to our List
+            dao.add(courseIdea);
+            //redirect the user to /ideas again, this time it displays also our List of courseIdea
+            response.redirect("/ideas");
+            return null;
+        });
     }
 }
